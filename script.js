6baa5a3a96a0fcd688668d3b8f5999982b5ff4c5
Mostafa Tahajomi <144267138+Mostafa-1676@users.noreply.github.com>
2025-05-10 04:06:22 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// let light_icon = document.getElementById("light_icon");
+
+// light_icon.onclick = function() {
+//     document.body.classList.toggle("light_mode");
+//     if (document.body.classList.contains("light_mode")) {
+//     }
+// }
+
+// type
+
+// let typed = new Typed(".multiple_text", {
+//     strings: ["Front-End Developer", "Graphic Designer", "Engineering Student"],
+//     typeSpeed: 64,
+//     backSpeed: 32,
+//     backDelay: 2048,
+//     loop: true,
+// }) 
